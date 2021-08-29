17615150236ecf567ae9fa51c71ae153993c2c2d
Matheus Gomes <matheus081105@gmail.com>
2021-08-28 21:39:26 -0300

    .

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
 $(function(){
-    
+
+    let errorMessege = 'Combinação inválida. Não foi possível calcular'
+
+    $('#convert').submit(function (e) { 
+        e.preventDefault();
+
+        let mVerification;
+        let kmVerification;
+        let m = $('#m').val();
+        let km = $('#km').val();
+
+        if(m == '' && km > 0) {
+             kmVerification = true;
+             mVerification = false;
+             console.log('certo1')
+             
+        } else if (m > 0 && km == '') {
+            mVerification = true;
+            kmVerification = false;
+            console.log('certo2')
+
+        } else if (m > 0 && km > 0) {
+            alert(errorMessege)
+            console.log('certo3')
+
+        } else if (m == '' && km == '') {
+            alert(errorMessege)
+            console.log('certo4')
+            
+        }
+        
+    });
 })  
