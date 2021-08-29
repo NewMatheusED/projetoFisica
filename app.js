51f6fd370b2c14621ef63add050a9846ea5f53fb
Matheus <matheus081105@gmail.com>
2021-08-28 23:27:04 -0300

    .

--- app.js
+++ app.js
@@ -2,31 +2,51 @@ $(function(){
 
     let errorMessege = 'Combinação inválida. Não foi possível calcular'
 
+    function mToKm (kilom) {
+        return (kilom / 3.6).toFixed(2)
+    }
+
+    function kmToM (metro) {
+        return (metro * 3.6).toFixed(2)
+    }
+
+    function reset(el) {
+        el.val('')
+    }
+
+    $('input[type=text]').focus(function() {
+        reset($(this))
+    })
+
     $('#convert').submit(function (e) { 
         e.preventDefault();
 
-        let mVerification;
-        let kmVerification;
+        // let mVerification;
+        // let kmVerification;
         let m = $('#m').val();
         let km = $('#km').val();
 
         if(m == '' && km > 0) {
-             kmVerification = true;
-             mVerification = false;
-             console.log('certo1')
+            //  kmVerification = true;
+            //  mVerification = false;
+             $('#m').val(mToKm(km));
+             $('#paragraph').text(`${km} km/h equivale a ${mToKm(km)} m/s`)
+             //console.log('certo1')
              
         } else if (m > 0 && km == '') {
-            mVerification = true;
-            kmVerification = false;
-            console.log('certo2')
+            // mVerification = true;
+            // kmVerification = false;
+            $('#km').val(kmToM(m));
+            $('#paragraph').text(`${m} m/s equivale a ${kmToM(m)} km/h`)
+            //console.log('certo2')
 
         } else if (m > 0 && km > 0) {
             alert(errorMessege)
-            console.log('certo3')
+            //console.log('certo3')
 
         } else if (m == '' && km == '') {
             alert(errorMessege)
-            console.log('certo4')
+            //console.log('certo4')
             
         }
         
